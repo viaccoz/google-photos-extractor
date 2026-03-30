56dc3d0c6ef1812953bc9bd450f63707ed30ff38
Thierry Viaccoz <42694303+viaccoz@users.noreply.github.com>
2026-03-30 21:40:19 +0200

    Update google-photos-extractor.py

--- google-photos-extractor.py
+++ google-photos-extractor.py
@@ -29,8 +29,11 @@ REGEXP_EXACT_YEAR = re.compile(r'\s*\b((?:19|20)\d{2})\b\s*')
 
 def sanitize_description(description: str) -> str:
     description = description.strip()
-    description = sanitize_filename(description)
+    description = description.replace('oeu', 'œu')
+    description = re.sub(r' -([^ ])', ' - \\1', description)
+    description = re.sub(r'([^ ])- ', '\\1 - ', description)
     description = re.sub(r'\s{2,}', ' ', description)
+    description = sanitize_filename(description)
     return description
 
 def sanitize_directory(directory: str) -> str:
@@ -72,7 +75,7 @@ def extract_sort_date_and_description(description: str) -> str:
     match = REGEXP_EXACT_SEASON.search(description)
     if match:
         season, year = match.groups()
-        return format_date(year, SEASON.get(season)), get_description_without_date(description, REGEXP_EXACT_SEASON)
+        return format_date(year, SEASONS.get(season)), get_description_without_date(description, REGEXP_EXACT_SEASON)
 
     # Exact year
     match = REGEXP_EXACT_YEAR.search(description)
@@ -91,7 +94,8 @@ with zipfile.ZipFile(zip_path, 'r') as zf:
     for file in target_files:
         if file.endswith('.json'):
             json_path = Path(file)
-            base_path = json_path.with_suffix('').with_suffix('').as_posix() # Remove ".supplemental-metadata.json" or similar
+            base_path = json_path.as_posix()
+            base_path = re.sub(r'(?:\.supp?l?e?m?e?n?t?a?l?-?m?e?t?a?d?a?t?a?)?\.json$', '', base_path)
 
             with zf.open(file) as jf:
                 try:
@@ -100,8 +104,11 @@ with zipfile.ZipFile(zip_path, 'r') as zf:
                     if description:
                         sort_date, description = extract_sort_date_and_description(description)
                         sanitized_description = sanitize_description(description)
-                        if description.strip() != sanitized_description:
+                        if description.rstrip() != sanitized_description:
                             print(f'[*] Sanitized description:\n      {description}\n      {sanitized_description}')
+                        if re.search(r'/original_[^/]+_$', base_path):
+                            print(f'[*] Dirty fix for original_guid_.json: {base_path}')
+                            base_path += 'P.jpg'
                         sort_dates_and_descriptions[base_path] = sort_date, sanitized_description
                 except json.JSONDecodeError:
                     print(f'[-] Ignored invalid JSON: {file}')
@@ -110,10 +117,10 @@ with zipfile.ZipFile(zip_path, 'r') as zf:
 
     files_to_remove = set()
     for file in target_files:
-        match = re.search(r'^(.+)-modifié(\.[^.]+)$', file)
+        match = re.search(r'^(.+)(?:-modifié|\(1\))(\.[^.]+)$', file)
         if match:
             original_file = match.group(1) + match.group(2)
-            if original_file in sort_dates_and_descriptions:
+            if file not in sort_dates_and_descriptions and original_file in sort_dates_and_descriptions:
                 sort_dates_and_descriptions[file] = sort_dates_and_descriptions.pop(original_file)
                 files_to_remove.add(original_file)
                 print(f'[*] Removed original image:\n      {original_file}\n      {file}')
@@ -124,22 +131,23 @@ with zipfile.ZipFile(zip_path, 'r') as zf:
         if not file.endswith('.json') and not file.endswith('/') and file not in files_to_remove:
             file_path = Path(file)
 
-            sort_date, description = sort_dates_and_descriptions.get(file_path.as_posix(), (None, None))
+            sort_date, description = sort_dates_and_descriptions.pop(file_path.as_posix(), (None, None))
             if sort_date and description:
                 target_file_name = f'{sort_date}___{description}___{file_path.stem}{file_path.suffix}'
             else:
+                print(f'[-] Missing data for: {file_path}')
                 target_file_name = f'XXXX-XX-XX___-___{file_path.stem}{file_path.suffix}'
 
             directory = str(file_path.parent)
             sanitized_directory = sanitize_directory(directory)
             if directory != sanitized_directory:
-                print(f'[*] Sanitized directory:\n      {directory}\n      {sanitized_directory}')
+                print(f'[-] Sanitized directory:\n      {directory}\n      {sanitized_directory}')
 
             current_target_directory = target_directory / sanitized_directory
             current_target_directory.mkdir(parents=True, exist_ok=True)
             target_path = current_target_directory / target_file_name
 
-            print(f'[*] Extracting image: {target_path}')
+            #print(f'[*] Extracting image: {target_path}')
 
             if len(str(target_path)) > 256:
                 print(f'[-] Path exceeds length limit: {target_path}')
@@ -147,3 +155,6 @@ with zipfile.ZipFile(zip_path, 'r') as zf:
             with zf.open(file) as source, open(target_path, 'wb') as target:
                 target.write(source.read())
                 #print(f'[+] Extracted image: {file_path.name}')
+
+    for base_path in sort_dates_and_descriptions.keys():
+        print(f'[-] Unused description for: {base_path}')
